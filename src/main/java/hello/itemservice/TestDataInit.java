128805a05d14a6d2cde1e1a868fcc0a4eb8d6f33
parkchunsu <pcc0105@gmail.com>
2023-06-22 18:38:38 +0900

    first

---
+++ src/main/java/hello/itemservice/TestDataInit.java
@@ -0,0 +1,24 @@
+package hello.itemservice;
+
+import org.springframework.stereotype.*;
+
+import hello.itemservice.domain.item.*;
+import jakarta.annotation.*;
+import lombok.*;
+
+@Component
+@RequiredArgsConstructor
+public class TestDataInit {
+
+    private final ItemRepository itemRepository;
+
+    /**
+     * 테스트용 데이터 추가
+     */
+    @PostConstruct
+    public void init() {
+        itemRepository.save(new Item("itemA", 10000, 10));
+        itemRepository.save(new Item("itemB", 20000, 20));
+    }
+
+}
---
+++ src/main/java/hello/thymeleaf/basic/TemplateController.java
@@ -0,0 +1,25 @@
+package hello.thymeleaf.basic;
+
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/template")
+public class TemplateController {
+
+	@GetMapping("/fragment")
+	public String template() {
+		return "template/fragment/fragmentMain";
+	}
+
+	@GetMapping("/layout")
+	public String layout() {
+		return "template/layout/layoutMain";
+	}
+
+	@GetMapping("/layoutExtend")
+	public String layoutExtends() {
+		return "template/layoutExtend/layoutExtendMain";
+	}
+
+}
